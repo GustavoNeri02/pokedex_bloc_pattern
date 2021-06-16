003253973de119863320049a51755e82aac0bd30
Gustavo Neri Pereira Dias <gutneri@hotmail.com>
2021-06-15 21:13:48 -0300

    novos estilos de pokebola, extendbody sacaffold

--- lib/app/screens/home/components/custom_appbar.dart
+++ lib/app/screens/home/components/custom_appbar.dart
@@ -1,72 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_bloc_pattern/app/bloc/app_decoration_bloc.dart';
 import 'package:pokedex_bloc_pattern/app/bloc/app_module.dart';
-
-List<BoxDecoration> pokebolas = [
-  BoxDecoration(
-      borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
-      border: Border.all(),
-      gradient: LinearGradient(
-        colors: [Colors.red, Colors.black, Colors.white],
-        stops: [0.45, 0.5, 0.55],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-      )),
-  BoxDecoration(
-      borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
-      border: Border.all(),
-      gradient: LinearGradient(
-        colors: [Colors.yellow, Colors.black, Colors.white],
-        stops: [0.45, 0.5, 0.55],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-      )),
-  BoxDecoration(
-      borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
-      border: Border.all(),
-      gradient: LinearGradient(
-        colors: [Colors.blue, Colors.black, Colors.white],
-        stops: [0.45, 0.5, 0.55],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-      )),
-];
+import 'package:pokedex_bloc_pattern/app/screens/home/components/pokebolas.dart';
 
 AppBar customAppBar(BuildContext context) {
   return AppBar(
     shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(50))),
+        borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+        ),
+    ),
     flexibleSpace: StreamBuilder<int>(
         initialData: 0,
         stream: AppModule.to.bloc<AppDecorationBloc>().index,
         builder: (context, snapshot) {
-          return Container(
-            decoration: pokebolas[snapshot.data!],
-            child: Center(
-              child: GestureDetector(
-                onTap: () {
-                  AppModule.to.bloc<AppDecorationBloc>().changePokebol();
-                },
-                child: Container(
-                  width: 30,
-                  height: 30,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(50),
-                      border: Border.all(width: 3),
-                      color: Colors.white),
-                  child: Center(
-                    child: Container(
-                        width: 15,
-                        height: 15,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: Colors.white,
-                            border: Border.all(width: 1.5))),
-                  ),
-                ),
-              ),
-            ),
-          );
+          return pokeballs(snapshot.data!);
         }),
   );
 }
---
+++ lib/app/screens/home/components/pokebolas.dart
@@ -0,0 +1,204 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:pokedex_bloc_pattern/app/bloc/app_decoration_bloc.dart';
+import 'package:pokedex_bloc_pattern/app/bloc/app_module.dart';
+
+Widget pokeballs(int index) {
+  switch (index) {
+    case 0:
+      //pokeball
+      return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
+            border: Border.all(),
+            gradient: LinearGradient(
+              colors: [Colors.red, Colors.black, Colors.white],
+              stops: [0.45, 0.5, 0.55],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )),
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              AppModule.to.bloc<AppDecorationBloc>().changePokebol();
+            },
+            child: Container(
+              width: 30,
+              height: 30,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  border: Border.all(width: 3),
+                  color: Colors.white),
+              child: Center(
+                child: Container(
+                    width: 15,
+                    height: 15,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.white,
+                        border: Border.all(width: 1.5))),
+              ),
+            ),
+          ),
+        ),
+      );
+    case 1:
+      //greatball
+      return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
+            border: Border.all(),
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.black, Colors.white],
+              stops: [0.45, 0.5, 0.55],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )),
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              AppModule.to.bloc<AppDecorationBloc>().changePokebol();
+            },
+            child: Stack(
+              children: [
+                Positioned(
+                  top: -15,
+                  left: 80,
+                  child: Transform.rotate(
+                    angle: -pi/5,
+                    child: Container(
+                      height: 45,
+                      width: 18,
+                      decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                          )
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: -15,
+                  right: 80,
+                  child: Transform.rotate(
+                    angle: pi/5,
+                    child: Container(
+                      height: 45,
+                      width: 18,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(10),
+                            bottomRight: Radius.circular(10),
+                        )
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  heightFactor: 2.5,
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(width: 3),
+                        color: Colors.white),
+                    child: Center(
+                      child: Container(
+                          width: 15,
+                          height: 15,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white,
+                              border: Border.all(width: 1.5))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    case 2:
+      //ultraball
+      return Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(50)),
+            border: Border.all(),
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.black, Colors.white],
+              stops: [0.45, 0.5, 0.55],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            )),
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              AppModule.to.bloc<AppDecorationBloc>().changePokebol();
+            },
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 80,
+                  child: Container(
+                    height: 20,
+                    width: 40,decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      )
+                  ),
+
+
+                  ),
+                ),
+                Positioned(
+                  right: 80,
+                  child: Container(
+                    height: 20,
+                    width: 40,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        )
+                    ),
+                  ),
+                ),
+                Center(
+                  heightFactor: 2.5,
+                  child: Container(
+                    width: 30,
+                    height: 30,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border.all(width: 3),
+                        color: Colors.white),
+                    child: Center(
+                      child: Container(
+                          width: 15,
+                          height: 15,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: Colors.white,
+                              border: Border.all(width: 1.5))),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    default:
+      return Container();
+  }
+}
